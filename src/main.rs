c5129afc989927e3225a7292911be4faf0940182
Blake Sweeney <bsweeney@ebi.ac.uk>
2020-07-27 21:40:30 +0100

    Add version with different XML parser
    This uses a different parser, quick-xml because it has examples how to
    read text. We need to do that in order to ensure the text we are
    counting is valid. This is still pretty fast, and in fact is faster than
    the svg parser based one.

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,10 @@ use std::iter::Iterator;
 
 use glob::glob;
 
-use svg::parser::Event;
+// use svg::parser::Event;
+
+use quick_xml::Reader;
+use quick_xml::events::Event;
 
 use serde::{Serialize, Deserialize};
 
@@ -34,37 +37,97 @@ struct Counts {
     total: u64,
 }
 
+fn is_valid_letter(text: String) -> bool {
+    match text.as_ref() {
+        "A" | "C" | "G" | "U" | "X" => true,
+        _ => false,
+    }
+}
+
 fn count(path: &Path) -> Result<Counts, Box<dyn Error>> {
     let urs = path.file_stem().unwrap().to_str().unwrap().to_string();
-    let mut counts = Counts { urs, changed: 0, unchanged: 0, inserted: 0, moved: 0, rotated: 0, total: 0 };
-    let doc = svg::open(path)?;
-
-    for event in doc {
-        match event {
-            Event::Tag("text", _, attributes) => {
-                let class = attributes.get("class");
-                match class {
-                    None => (),
-                    Some(v) => match v.as_ref() {
-                        "green" => counts.changed += 1,
-                        "black" => counts.unchanged += 1,
-                        "red" => counts.inserted += 1,
-                        "blue" => counts.moved += 1,
-                        "brown" => counts.rotated += 1,
-                        _ => (),
-                    }
+    let mut counts = Counts {
+        urs,
+        changed: 0,
+        unchanged: 0,
+        inserted: 0,
+        moved: 0,
+        rotated: 0,
+        total: 0,
+    };
+    let mut buf = Vec::new();
+    let mut reader = Reader::from_file(path)?;
+    loop {
+        match reader.read_event(&mut buf) {
+            Ok(Event::Start(ref e)) => {
+                match e.name() {
+                    b"text" => {
+                        let text = reader.read_text(b"text", &mut Vec::new())?;
+                        let class = e.attributes().with_checks(false).find(|attr| {
+                            match attr {
+                                Ok(a) => a.key == b"class",
+                                _ => false,
+                            }
+                        });
+                        match (is_valid_letter(text), class) {
+                            (true, Some(Ok(v))) => match v.value.as_ref() {
+                                b"green" => counts.changed += 1,
+                                b"black" => counts.unchanged += 1,
+                                b"red" => counts.inserted += 1,
+                                b"blue" => counts.moved += 1,
+                                b"brown" => counts.rotated += 1,
+                                _ => (),
+                            },
+                            _ => (),
+                        }
+                    },
+                    _ => (),
                 }
             },
+            Err(e) => panic!("Error at position {}: {:?}", reader.buffer_position(), e),
+            Ok(Event::Eof) => break,
             _ => (),
         }
     }
-    counts.total = counts.changed + 
-        counts.unchanged + 
-        counts.inserted + 
-        counts.moved + 
+    counts.total = counts.changed +
+        counts.unchanged +
+        counts.inserted +
+        counts.moved +
         counts.rotated;
     return Ok(counts);
 }
+
+// fn count(path: &Path) -> Result<Counts, Box<dyn Error>> {
+//     let urs = path.file_stem().unwrap().to_str().unwrap().to_string();
+//     let mut counts = Counts { urs, changed: 0, unchanged: 0, inserted: 0, moved: 0, rotated: 0, total: 0 };
+//     let doc = svg::open(path)?;
+
+//     for event in doc {
+//         match event {
+//             Event::Tag("text", _, attributes) => {
+//                 let class = attributes.get("class");
+//                 match class {
+//                     None => (),
+//                     Some(v) => match v.as_ref() {
+//                         "green" => counts.changed += 1,
+//                         "black" => counts.unchanged += 1,
+//                         "red" => counts.inserted += 1,
+//                         "blue" => counts.moved += 1,
+//                         "brown" => counts.rotated += 1,
+//                         _ => (),
+//                     }
+//                 }
+//             },
+//             _ => (),
+//         }
+//     }
+//     counts.total = counts.changed +
+//         counts.unchanged +
+//         counts.inserted +
+//         counts.moved +
+//         counts.rotated;
+//     return Ok(counts);
+// }
 
 pub fn main() -> Result<(), Box<dyn Error>> {
     let opt = Opt::from_args();
