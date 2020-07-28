de02dc389ede235f6a588b51f5cf84b6dc2d2ad4
Blake Sweeney <bsweeney@ebi.ac.uk>
2020-07-28 20:06:52 +0100

    Process trees or json files
    We can now process either a file with JSON object per line with urs and
    svg keys as well as walk a directory. Testing this shows that in release
    mode this takes 15 seconds on a test data set while the python version
    takes 8 minutes.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,12 @@
-// #[macro_use]
-// extern crate structopt;
-
-extern crate glob;
-
-use std::io;
-use std::path::Path;
 use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::{self, BufReader};
 use std::iter::Iterator;
+use std::path::{Path, PathBuf};
 
-use glob::glob;
-
-// use svg::parser::Event;
+use walkdir::WalkDir;
+use globset::Glob;
 
 use quick_xml::Reader;
 use quick_xml::events::Event;
@@ -20,10 +16,24 @@ use serde::{Serialize, Deserialize};
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
+enum Command {
+    #[structopt(name = "tree", about = "Iterate over a tree and find parse all SVGS")]
+    Tree {
+        #[structopt(parse(from_os_str))]
+        tree: PathBuf,
+    },
+    #[structopt(name = "json-file", about="Parse a JSON file of urs, layout of SVGS")]
+    Json {
+        #[structopt(parse(from_os_str))]
+        file: PathBuf
+    },
+}
+
+#[derive(StructOpt)]
 #[structopt(about = "Tool to parse a tree of SVG files and do color counts")]
 struct Opt {
-    /// Input file
-    input: String,
+    #[structopt(subcommand)]
+    cmd: Command
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -37,6 +47,12 @@ struct Counts {
     total: u64,
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+struct JsonEntry {
+    urs: String,
+    svg: String,
+}
+
 fn is_valid_letter(text: String) -> bool {
     match text.as_ref() {
         "A" | "C" | "G" | "U" | "X" => true,
@@ -44,8 +60,7 @@ fn is_valid_letter(text: String) -> bool {
     }
 }
 
-fn count(path: &Path) -> Result<Counts, Box<dyn Error>> {
-    let urs = path.file_stem().unwrap().to_str().unwrap().to_string();
+fn count_reader<B: BufRead>(urs: String, reader: &mut Reader<B>) -> Result<Counts, Box<dyn Error>> {
     let mut counts = Counts {
         urs,
         changed: 0,
@@ -56,13 +71,12 @@ fn count(path: &Path) -> Result<Counts, Box<dyn Error>> {
         total: 0,
     };
     let mut buf = Vec::new();
-    let mut reader = Reader::from_file(path)?;
     loop {
         match reader.read_event(&mut buf) {
             Ok(Event::Start(ref e)) => {
                 match e.name() {
                     b"text" => {
-                        let text = reader.read_text(b"text", &mut Vec::new())?;
+                        let text = reader.read_text(e.name(), &mut Vec::new())?;
                         let class = e.attributes().with_checks(false).find(|attr| {
                             match attr {
                                 Ok(a) => a.key == b"class",
@@ -97,50 +111,49 @@ fn count(path: &Path) -> Result<Counts, Box<dyn Error>> {
     return Ok(counts);
 }
 
-// fn count(path: &Path) -> Result<Counts, Box<dyn Error>> {
-//     let urs = path.file_stem().unwrap().to_str().unwrap().to_string();
-//     let mut counts = Counts { urs, changed: 0, unchanged: 0, inserted: 0, moved: 0, rotated: 0, total: 0 };
-//     let doc = svg::open(path)?;
-
-//     for event in doc {
-//         match event {
-//             Event::Tag("text", _, attributes) => {
-//                 let class = attributes.get("class");
-//                 match class {
-//                     None => (),
-//                     Some(v) => match v.as_ref() {
-//                         "green" => counts.changed += 1,
-//                         "black" => counts.unchanged += 1,
-//                         "red" => counts.inserted += 1,
-//                         "blue" => counts.moved += 1,
-//                         "brown" => counts.rotated += 1,
-//                         _ => (),
-//                     }
-//                 }
-//             },
-//             _ => (),
-//         }
-//     }
-//     counts.total = counts.changed +
-//         counts.unchanged +
-//         counts.inserted +
-//         counts.moved +
-//         counts.rotated;
-//     return Ok(counts);
-// }
-
-pub fn main() -> Result<(), Box<dyn Error>> {
-    let opt = Opt::from_args();
-
-    let mut wtr = csv::Writer::from_writer(io::stdout());
-
-    let counts = glob(&opt.input)?
+fn count_tree(path: PathBuf) -> Result<(), Box<dyn Error>> {
+    let walker = WalkDir::new(path);
+    let glob = Glob::new("*.svg")?.compile_matcher();
+    let counts = walker.into_iter()
         .filter_map(Result::ok)
-        .map(|p| count(&p));
-
+        .filter(move |e| glob.is_match(e.file_name()))
+        .map(|path| {
+            let filename = Path::new(path.file_name());
+            let urs = filename.file_stem().unwrap().to_str().unwrap().to_string();
+            let mut reader = Reader::from_file(path.path())?;
+            return count_reader(urs, &mut reader);
+        });
+    let mut wtr = csv::Writer::from_writer(io::stdout());
     for count in counts {
         let c = count?;
         wtr.serialize(c)?;
     }
     return Ok(());
+}
+
+fn count_json(filename: PathBuf) -> Result<(), Box<dyn Error>> {
+    let file = File::open(filename)?;
+    let file = BufReader::new(file);
+    let counts = file.lines()
+        .into_iter()
+        .map(|line|  {
+            let line = line?.replace("\\\\", "\\");
+            let entry: JsonEntry = serde_json::from_str(&line)?;
+            let mut reader = Reader::from_str(entry.svg.as_ref());
+            return count_reader(entry.urs, &mut reader);
+        });
+    let mut wtr = csv::Writer::from_writer(io::stdout());
+    for count in counts {
+        let c = count?;
+        wtr.serialize(c)?;
+    }
+    return Ok(());
+}
+
+pub fn main() -> Result<(), Box<dyn Error>> {
+    let opt = Opt::from_args();
+    return match opt.cmd {
+        Command::Tree { tree } => count_tree(tree),
+        Command::Json { file } => count_json(file),
+    };
 }
